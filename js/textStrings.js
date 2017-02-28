1dd3087c1d8db0015f0ebec6c3746402925dc5ac
Greg Decroix <gregdecroix@gmail.com>
2017-02-28 16:41:46 +0100

    translation_FR
    Text translation done in french.
    
    (does "peep" stands for a sound or a word  or something else I don't catch ?)

--- js/textStrings.js
+++ js/textStrings.js
@@ -208,4 +208,107 @@ var textStrings_DE = {
     "misatrributed"      : "(falsch zugeschrieben)" ,
 };
 
+var textStrings_FR = {
+    // Act 1
+    "niceHat"              : "Ooh quel joli chapeau"                                          ,
+    "outtaHere"            : "yeah git on outta here"                                ,
+    "getARoom"             : "BEURK, trouvez vous une chambre"                                  ,
+    "notCoolAnymore"       : "Les chapeaux ne sont plus à la mode"                          ,
+    "tvOnTv"               : "Une télé...A la télé !"                                        ,
+    "cricky"               : "Pti criquet <3"                                        ,
+    "tooManyCrickets"      : "ok, trop de criquets !"                         ,
+    "normalPeep"           : "c'est juste un bruit normal"                                    ,
+    "normalPeeps"          : "simplement des bruits normaux"                                ,
+    "wowNothing"           : "Waaaa, ce n'est rien..."                                  ,
+    
+    // Act 2
+    "crazySquareAttacks"   : "ATTAQUE D'UN CARRÉ FOU"                                 ,
+    "justMissed"           : "oooooh, raté"                                 ,
+    "somethingInteresting" : "(tu dois les prendre en train de faire *quelque chose* d'intéressant)" ,
+    "whoIsScreaming"       : "(tu dois prendre celui qui leurs cri dessus)"               ,
+    "circleFearsSquares"   : "LES CERCLES SONT EFFRAYES PAR LES CARRES"                                  ,
+    "whoScaresThem"        : "(tu dois aussi prendre *celui* qui les terrorise)"         ,
+    "areTheyScared"        : "(tu dois les prendre *en train d'être* terrorisés par un carré)"       , // not used
+    "squaresSnubCircles"   : "LES CARRES SNOBENT LES RONDS"                                  ,
+    "areTheySnubbed"       : "(tu dois les prendre *pendant* qu'ils snobent un cercle)"        , // not used
+    "everyoneHates"        : "TOUT LE MONDE DÉTESTE TOUT LE MONDE !!1!"                           ,
+    "almostEveryoneHates"  : "PRESQUE TOUT LE MONDE DÉTESTE TOUT LE MONDE ..."                     ,
+    "squaresHateCircles"   : "LES CARRÉS DÉTESTENT LES CERCLES"                                  ,
+    "circlesHateSquares"   : "LES CERCLES DÉTESTENT LES CARRÉS"                                  ,
+    "areTheyYelling"       : "(tu dois les prendre en train *d'hurler* entre eux)"              , // not used
+    "nerdsNow"             : "que font ces intellos en ce moment"                        ,
+    "schockedPeep"         : "pourquoi ce peep est choqué?"                              ,
+    "whatever"             : "peu importe"                                              ,
+    
+    // Act 3
+    "ellipsis"             : ". . ."                                                 ,
+    "coolNoMore"           : "les chapeaux ne sont plus à la mode, vous ne le savez pas?"           ,
+    "beScared"             : "SOIS TERRORISÉ, SOIS EN COLÈRE."                                  ,
+    
+    // MANIFESTO
+    "manifesto" : [
+       //"as if you viewers want GOOD news"       ,
+       "qui se connecte pour regarder des *gens s'entendre?*",
+       "la paix est d'un ennui, la violence fait le buzz."    ,
+       //"peace is boring. conflict gets clicks." ,
+       "et chaque histoire a besoin d'un conflit, alors..."  ,
+       //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
+       "DONNE AU PUBLIC CE QU'IL VEUT."
+    ] ,
+    
+    // -- Strings currently in graphics resources --
+    
+    // (bg_preload.png) Preload screen
+    "playingTime"        : "temps de jeu: 5 minutes" ,
+    "warning"            : "attention: le programme contient des scènes de dénigrement, de violence verbale et physique et notamment, de meurtres de masse.\n ce programme s'adresse à un public majeur averti." ,
+    
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"      : "POINTEZ & CLIQUEZ" ,
+    
+    // (chyron3.png)
+    "chyronNothing"      : "WAAAA, CE N'EST *RIEN*" ,
+    
+    // (credits0001.png)
+    "createdBy"          : "créé par" ,
+    "NickyCase"          : "Nicky Case" ,
+    
+    // (credits0002.png)
+    "manyThanks"         : "mille mercis à mes testeurs:" ,
+    
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"  : "et à mes soutiens sur Patreons:" ,
+    
+    // (credits0007.png)
+    "lastButNotLeast"    : "et pour finir," ,
+    
+    // (credits0008.png)
+    "thankYouForPlaying" : "merci à VOUS d'avoir joué!" ,
+    
+    // (preload_play.png)
+    "playButton"         : "JOUER" ,
+    
+    // (end_button.png) Post Credits
+    "otherWorkButton"    : "mes autres réalisations" ,
+    "buyCoffeeButton"    : "paye moi un café" ,
+    "replayButton"       : "rejoue ce truc" ,
+    
+    // (logo.png) Post Credits
+    "logoWBWWB"          : "PARTAGE LA DOULEUR:" ,
+    
+    // (end_prototype.png)
+    "endOfPrototype"     : "FIN DU PROTOTYPE" ,
+    "toBeContinued"      : "(à suivre!)" ,
+    
+    // (quote0002.png)
+    "WBWWB"              : "Nous devenons ce que nous regardons." ,
+    "WSOTATOTSU"         : "Les outils que nous créons, finissent par nous façonner." ,
+    
+    // (quote0003.png)
+    "MarshallMcLuhan"    : "Marshall McLuhan" ,
+    
+    // (quote0004.png)
+    "misatrributed"      : "(attribué par erreur)" ,
+    
+};
+
 var textStrings = textStrings_EN
