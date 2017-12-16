a0973590c1a08cf4a15693b20872035025c6ef14
Daniel Król <daniel@krol.me>
2017-12-16 19:15:46 +0100

    Add PL translation

--- js/textStrings.js
+++ js/textStrings.js
@@ -517,4 +517,103 @@ var textStrings_SV = {
     "misatrributed"      : "(felciterad)" ,
 };
 
+var textStrings_PL = {
+    // Act 1
+    "niceHat"              : "OOH FAJNY KAPELUSZ!"                                          ,
+    "outtaHere"            : "właśnie, wynoś się"                                           ,
+    "getARoom"             : "Serio, tak przy ludziach?"                                    ,
+    "notCoolAnymore"       : "kapelusze już nie są spoko"                                   ,
+    "tvOnTv"               : "Telewizor... w telewizji!"                                    ,
+    "cricky"               : "ŚWIERSZCZYK <3"                                               ,
+    "tooManyCrickets"      : "dobra, starczy tych świerszczy"                               ,
+    "normalPeep"           : "zwykły ludek"                                                 ,
+    "normalPeeps"          : "jacyś ludzie"                                                 ,
+    "wowNothing"           : "WOW! To... nic "                                              ,
+
+    // Act 2
+    "crazySquareAttacks"   : "SZALONY KWADRAT ATAKUJE"                                      ,
+    "justMissed"           : "ahhh, przegapiłeś to"                                         ,
+    "somethingInteresting" : "(musisz go złapać robiącego *cokolwiek* interesującego...)"   ,
+    "whoIsScreaming"       : "(musisz złapać *kto* krzyczy)"                                ,
+    "circleFearsSquares"   : "KOŁA BOJĄ SIĘ KWADRATÓW"                                      ,
+    "whoScaresThem"        : "(musisz złapać *kogo* się boi)"                               ,
+    "areTheyScared"        : "(musisz ich złapać *straszonych* przez kwadrat)"              , // not used
+    "squaresSnubCircles"   : "KWADRATY GARDZĄ KOŁAMI"                                       ,
+    "areTheySnubbed"       : "(musisz ich złapać *gardzących* kołami)"                      , // not used
+    "everyoneHates"        : "WSZYSCY NIENAWIDZĄ WSZYSTKICH!!1!"                            ,
+    "almostEveryoneHates"  : "PRAWIE WSZYSCY NIENAWIDZĄ WSZYSTKICH..."                      ,
+    "squaresHateCircles"   : "KWADRATY NIENAWIDZĄ KÓŁ"                                      ,
+    "circlesHateSquares"   : "KOŁA NIENAWIDZĄ KWADRATÓW"                                    ,
+    "areTheyYelling"       : "(musisz ich złapać *krzyczących* na innych)"                  , // not used
+    "nerdsNow"             : "co ci frajerzy robią?"                                        ,
+    "schockedPeep"         : "czemu ten ludek jest wstrząśnięty?"                           ,
+    "whatever"             : "cokolwiek"                                                    ,
+
+    // Act 3
+    "ellipsis"             : ". . ."                                                        ,
+    "coolNoMore"           : "kapelusze już nie są modne, nie słyszałeś?"                   ,
+    "beScared"             : "BĄDŹ PRZESTRASZONY. BĄDŹ WŚCIEKŁY."                           ,
+
+    // MANIFESTO
+    "manifesto" : [
+       "kto ogląda *ludzi* którzy się godzą?",
+       "spokój jest nudny, dawaj przemoc",
+       "i każda opowieść potrzebuje konfliktu, więc...",
+       "DAJ WIDOWNI CZEGO CHCĄ."
+    ] ,
+
+    // -- Strings currently in graphics resources --
+
+    // (bg_preload.png) Preload screen
+    "playingTime"        : "czas gry: 5 minut" ,
+    "warning"            : "uwaga: ta gra\nzawiera sceny snobizmu,\ngrubiaństwa i masowego mordu.\ntreść może być\nnieodpowiednia dla dzieci." ,
+
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"      : "WSKAŻ I KLIKNIJ" ,
+
+    // (chyron3.png)
+    "chyronNothing"      : "YAAY, TO... *NIC*" ,
+
+    // (credits0001.png)
+    "createdBy"          : "created by" ,
+    "NickyCase"          : "Nicky Case" ,
+
+    // (credits0002.png)
+    "manyThanks"         : "wielkie dzięki dla moich testerów:" ,
+
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"  : "i tych ktorzy mnie wsparli na patreon:" ,
+
+    // (credits0007.png)
+    "lastButNotLeast"    : "i nie mniej ważne," ,
+
+    // (credits0008.png)
+    "thankYouForPlaying" : "dziękuję TOBIE za grę!" ,
+
+    // (preload_play.png)
+    "playButton"         : "GRAJ" ,
+
+    // (end_button.png) Post Credits
+    "otherWorkButton"    : "zobacz moje inne dzieła" ,
+    "buyCoffeeButton"    : "kup mi kawę" ,
+    "replayButton"       : "zagraj jeszcze raz" ,
+
+    // (logo.png) Post Credits
+    "logoWBWWB"          : "PODZIEL SIĘ BÓLEM:" ,
+
+    // (end_prototype.png)
+    "endOfPrototype"     : "KONIEC PROTOTYPU" ,
+    "toBeContinued"      : "(ciąg dalszy nastąpi!)" ,
+
+    // (quote0002.png)
+    "WBWWB"              : "Stajemy się tym co widzimy." ,
+    "WSOTATOTSU"         : "Kształtujemy nasze narzędzia, a następnie nasze narzędzia kształtują nas." ,
+
+    // (quote0003.png)
+    "MarshallMcLuhan"    : "Marshall McLuhan" ,
+
+    // (quote0004.png)
+    "misatrributed"      : "(błędnie oznaczony)"
+};
+
 var textStrings = textStrings_EN
