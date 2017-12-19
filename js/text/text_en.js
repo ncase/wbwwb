b3c33425270c3325ee9b0b7e764e3b0bae13ed25
Daniel Król <daniel@krol.me>
2017-12-19 09:59:26 +0100

    Split translations
    Use Object.assign to fill missing translations using base(english) text

---
+++ js/text/text_en.js
@@ -0,0 +1,108 @@
+///////////////////////////////////
+///// TEXT STRINGS ////////////////
+///////////////////////////////////
+
+var textStrings_EN = {
+    // Act 1
+    "niceHat"              : "OOH NICE HAT"                                          ,
+    "outtaHere"            : "yeah git on outta here"                                ,
+    "getARoom"             : "GROSS, GO GET A ROOM"                                  ,
+    "notCoolAnymore"       : "nvm hats aren't cool anymore"                          ,
+    "tvOnTv"               : "A TV... ON TV!"                                        ,
+    "cricky"               : "LIL' CRICKY <3"                                        ,
+    "tooManyCrickets"      : "okay that's too many crickets"                         ,
+    "normalPeep"           : "just a normal peep"                                    ,
+    "normalPeeps"          : "just some normal peeps"                                ,
+    "wowNothing"           : "WOWWEE, IT'S NOTHING"                                  ,
+
+    // Act 2
+    "crazySquareAttacks"   : "CRAZED SQUARE ATTACKS"                                 ,
+    "justMissed"           : "oooooh just missed it"                                 ,
+    "somethingInteresting" : "(ya gotta catch 'em doing *something* interesting...)" ,
+    "whoIsScreaming"       : "(ya gotta catch who's screaming at 'em)"               ,
+    "circleFearsSquares"   : "CIRCLE FEARS SQUARES"                                  ,
+    "whoScaresThem"        : "(ya gotta also catch *who* they're scared by)"         ,
+    "areTheyScared"        : "(ya gotta catch 'em *being* scared by a square)"       , // not used
+    "squaresSnubCircles"   : "SQUARES SNUB CIRCLES"                                  ,
+    "areTheySnubbed"       : "(ya gotta catch 'em *while* snubbing a circle)"        , // not used
+    "everyoneHates"        : "EVERYONE HATES EVERYONE!!1!"                           ,
+    "almostEveryoneHates"  : "ALMOST EVERYONE HATES EVERYONE..."                     ,
+    "squaresHateCircles"   : "SQUARES HATE CIRCLES"                                  ,
+    "circlesHateSquares"   : "CIRCLES HATE SQUARES"                                  ,
+    "areTheyYelling"       : "(ya gotta catch 'em *yelling* at others)"              , // not used
+    "nerdsNow"             : "what are these nerds doing now"                        ,
+    "schockedPeep"         : "why's this peep shocked?"                              ,
+    "whatever"             : "whatever"                                              ,
+
+    // Act 3
+    "ellipsis"             : ". . ."                                                 ,
+    "coolNoMore"           : "hats ain't cool no more, haven't you heard?"           ,
+    "beScared"             : "BE SCARED. BE ANGRY."                                  ,
+
+    // MANIFESTO
+    "manifesto" : [
+        //"as if you viewers want GOOD news"       ,
+        "who tunes in to watch *people get along?*",
+        "peace is boring. violence goes viral."    ,
+        //"peace is boring. conflict gets clicks." ,
+        "and every story needs a conflict, so..."  ,
+        //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
+        "GIVE THE AUDIENCE WHAT THEY WANT."
+    ] ,
+
+    // -- Strings currently in graphics resources --
+
+    // (bg_preload.png) Preload screen
+    "playingTime"        : "playing time: 5 minutes" ,
+    "warning"            : "warning: the following program\ncontains scenes of snobbery,\nrudeness & mass murder.\nviewer discretion is advised." ,
+
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"      : "POINT & CLICK" ,
+
+    // (chyron3.png)
+    "chyronNothing"      : "WOWWEE, IT'S *NOTHING*" ,
+
+    // (credits0001.png)
+    "createdBy"          : "created by" ,
+    "NickyCase"          : "Nicky Case" ,
+
+    // (credits0002.png)
+    "manyThanks"         : "many thanks to my playtesters:" ,
+
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"  : "and to my patreon supporters:" ,
+
+    // (credits0007.png)
+    "lastButNotLeast"    : "and last but not least," ,
+
+    // (credits0008.png)
+    "thankYouForPlaying" : "thank YOU for playing!" ,
+
+    // (preload_play.png)
+    "playButton"         : "PLAY" ,
+
+    // (end_button.png) Post Credits
+    "otherWorkButton"    : "see my other work" ,
+    "buyCoffeeButton"    : "buy me a coffee" ,
+    "replayButton"       : "replay this mess" ,
+
+    // (logo.png) Post Credits
+    "logoWBWWB"          : "SHARE THE PAIN:" ,
+
+    // (end_prototype.png)
+    "endOfPrototype"     : "END OF PROTOTYPE" ,
+    "toBeContinued"      : "(to be continued!)" ,
+
+    // (quote0002.png)
+    "WBWWB"              : "We become what we behold." ,
+    "WSOTATOTSU"         : "We shape our tools and then our tools shape us." ,
+
+    // (quote0003.png)
+    "MarshallMcLuhan"    : "Marshall McLuhan" ,
+
+    // (quote0004.png)
+    "misatrributed"      : "(misattributed)" ,
+};
+
+//
+var textStrings = textStrings_EN;
