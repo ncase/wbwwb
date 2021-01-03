9b0d931899ede75e28f8cf02aca096503c0d5c3c
Galandrielle <76888409+Galandrielle@users.noreply.github.com>
2021-01-03 02:00:23 +0200

    Update textStrings.js

--- js/textStrings.js
+++ js/textStrings.js
@@ -2,315 +2,109 @@
 ///// TEXT STRINGS ////////////////
 ///////////////////////////////////
 
-var textStrings_EN = {
+// Greek Translation
+// By Galandrielle
+
+var textStrings_GR = {
 	// Act 1
-	"niceHat": "OOH NICE HAT",
-	"outtaHere": "yeah git on outta here",
-	"getARoom": "GROSS, GO GET A ROOM",
-	"notCoolAnymore": "nvm hats aren't cool anymore",
-	"tvOnTv": "A TV... ON TV!",
-	"cricky": "LIL' CRICKY <3",
-	"tooManyCrickets": "okay that's too many crickets",
-	"normalPeep": "just a normal peep",
-	"normalPeeps": "just some normal peeps",
-	"wowNothing": "WOWWEE, IT'S NOTHING",
+	"niceHat": "ΩΡΑΙΟ ΚΑΠΕΛΟ",
+	"outtaHere": "ναι φύγε από δω",
+	"getARoom": "ΑΗΔΙΑ, ΝΑ ΠΑΤΕ ΑΛΛΟΥ",
+	"notCoolAnymore": "ναι τα καπέλα δεν είναι πια κουλ",
+	"tvOnTv": "ΤΗΛΕΟΡΑΣΗ... ΣΤΗΝ ΤΗΛΕΟΡΑΣΗ!",
+	"cricky": "ΤΡΙΖΕΙ ΛΙΓΟ <3",
+	"tooManyCrickets": "οκ πολλά τριζόνια",
+	"normalPeep": " απλώς ένας κανονικός τύπος",
+	"normalPeeps": "απλώς μερικοί κανονικοί τύποι",
+	"wowNothing": "ΟΥΑΟΥ, ΕΙΝΑΙ ΤΟ ΤΙΠΟΤΑ",
 
 	// Act 2
-	"crazySquareAttacks": "CRAZED SQUARE ATTACKS",
-	"justMissed": "oooooh just missed it",
-	"somethingInteresting": "(ya gotta catch 'em doing *something* interesting...)",
-	"whoIsScreaming": "(ya gotta catch who's screaming at 'em)",
-	"circleFearsSquares": "CIRCLE FEARS SQUARES",
-	"whoScaresThem": "(ya gotta also catch *who* they're scared by)",
-	"areTheyScared": "(ya gotta catch 'em *being* scared by a square)", // not used
-	"squaresSnubCircles": "SQUARES SNUB CIRCLES",
-	"areTheySnubbed": "(ya gotta catch 'em *while* snubbing a circle)", // not used
-	"everyoneHates": "EVERYONE HATES EVERYONE!!1!",
-	"almostEveryoneHates": "ALMOST EVERYONE HATES EVERYONE...",
-	"squaresHateCircles": "SQUARES HATE CIRCLES",
-	"circlesHateSquares": "CIRCLES HATE SQUARES",
-	"areTheyYelling": "(ya gotta catch 'em *yelling* at others)", // not used
-	"nerdsNow": "what are these nerds doing now",
-	"schockedPeep": "why's this peep shocked?",
-	"whatever": "whatever",
+	"crazySquareAttacks": "ΤΡΕΛΕΣ ΕΠΙΘΕΣΕΙΣ ΤΕΤΡΑΓΩΝΩΝ",
+	"justMissed": "	ωωωωωω μόλις το έχασες",
+	"somethingInteresting": "(πρέπει να τους πιάσεις να κάνουν ΚΑΤΙ ενδιαφέρον...)",
+	"whoIsScreaming": "(πρέπει να πιάσεις αυτόν που τους φωνάζει)",
+	"circleFearsSquares": "ΚΥΚΛΟΣ ΦΟΒΑΤΑΙ ΤΑ ΤΕΤΡΑΓΩΝΑ",
+	"whoScaresThem": "(πρέπει επίσης να πιάσεις ΠΟΙΟΝ φοβούνται...)",
+	"areTheyScared": "(πρέπει να τους πιάσεις να ΦΟΒΟΥΝΤΑΙ από τετράγωνο)", // not used
+	"squaresSnubCircles": "ΤΑ ΤΕΤΡΑΓΩΝΑ ΣΝΟΜΠΑΡΟΥΝ ΤΟΥΣ ΚΥΚΛΟΥΣ",
+	"areTheySnubbed": "(πρέπει να τους πιάσεις ενώ σνομπάρουν κύκλο...)", // not used
+	"everyoneHates": "ΟΛΟΙ ΜΙΣΟΥΝ ΟΛΟΥΣ!!1!",
+	"almostEveryoneHates": " ΣΧΕΔΟΝ ΟΛΟΙ ΜΙΣΟΥΝ ΟΛΟΥΣ...",
+	"squaresHateCircles": "ΤΑ ΤΕΤΡΑΓΩΝΑ ΜΙΣΟΥΝ ΤΟΥΣ ΚΥΚΛΟΥΣ",
+	"circlesHateSquares": "ΟΙ ΚΥΚΛΟΙ ΜΙΣΟΥΝ ΤΑ ΤΕΤΡΑΓΩΝΑ"",
+	"areTheyYelling": "(πρέπει να τους πιάσεις να ΦΩΝΑΖΟΥΝ σε άλλους)", // not used
+	"nerdsNow": "τί κάνουν αυτοί οι σπασίκλες τώρα",
+	"schockedPeep": "γιατί είναι αυτός τ τύπος σοκαρισμένος;",
+	"whatever": "τέλος πάντων",
 
 	// Act 3
 	"ellipsis": ". . .",
-	"coolNoMore": "hats ain't cool no more, haven't you heard?",
-	"beScared": "BE SCARED. BE ANGRY.",
+	"coolNoMore": "τα καπέλα δεν είναι πια κουλ, δεν άκουσες;",
+	"beScared": "ΤΡΟΜΑΞΕ. ΘΥΜΩΣΕ.",
 
 	// MANIFESTO
 	"manifesto": [
-		//"as if you viewers want GOOD news"       ,
-		"who tunes in to watch *people get along?*",
-		"peace is boring. violence goes viral.",
-		//"peace is boring. conflict gets clicks." ,
-		"and every story needs a conflict, so...",
-		//"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
-		"GIVE THE AUDIENCE WHAT THEY WANT."
+		//"λες κι εσείς οι θεατές θέλετε ΚΑΛΑ νέα"       ,
+		"ποιος μπαίνει να δει *ανθρώπους να τα πηγαίνουν καλά*;",
+		"η ειρήνη είναι βαρετή. η βία γίνεται viral.",
+		//"η ειρήνη είναι βαρετή. η σύγκρουση φέρνει κλικς." ,
+		"και κάθε ιστορία χρειάζεται μια σύγκρουση, οπότε...",
+		//"...ΔΩΣΕ ΣΤΟ ΚΟΙΝΟ ΑΥΤΟ ΠΟΥ ΘΕΛΕΙ."   ,
+		"ΔΩΣΕ ΣΤΟ ΚΟΙΝΟ ΑΥΤΟ ΠΟΥ ΘΕΛΕΙ."
 	],
 
 	// -- Strings currently in graphics resources --
 
 	// (bg_preload.png) Preload screen
-	"playingTime": "playing time: 5 minutes",
-	"warning": "warning: the following program\ncontains scenes of snobbery,\nrudeness & mass murder.\nviewer discretion is advised.",
+	"playingTime": "χρόνος παιχνιδιού: 5 λεπτά",
+	"warning": "προειδοποίηση: το πρόγραμμα\nπεριέχει σκηνές ακαταδεξίας,\nαγένειας και μαζικής δολοφονίας.\nη σύνεση του θεατή είναι προτεινόμενη.",
 
 	// (cam-instructions.png) Camera Instructions
 	"pointAndClick": "POINT & CLICK",
 
 	// (chyron3.png)
-	"chyronNothing": "WOWWEE, IT'S *NOTHING*",
+	"chyronNothing": "ΟΥΑΟΥ, ΕΙΝΑΙ ΤΟ *ΤΙΠΟΤΑ*",
 
 	// (credits0001.png)
-	"createdBy": "created by",
-	"NickyCase": "Nicky Case",
+	"createdBy": "δημιουργός",
+	"NickyCase": "Νίκι Κέις",
 
 	// (credits0002.png)
-	"manyThanks": "many thanks to my playtesters:",
+	"manyThanks": " ευχαριστώ τους δοκιμαστές του παιχνιδιού:",
 
 	// (credits000[3,4,5,6].png)
-	"patreonSupporters": "and to my patreon supporters:",
+	"patreonSupporters": " και τους υποστηρικτές στο patreon:",
 
 	// (credits0007.png)
-	"lastButNotLeast": "and last but not least,",
+	"lastButNotLeast": "και τελευταία αλλά όχι καταϊδρωμένα,",
 
 	// (credits0008.png)
-	"thankYouForPlaying": "thank YOU for playing!",
+	"thankYouForPlaying": "ευχαριστώ ΕΣΕΝΑ που έπαιξες!",
 
 	// (preload_play.png)
-	"playButton": "PLAY",
+	"playButton": "ΠΑΙΞΕ",
 
 	// (end_button.png) Post Credits
-	"otherWorkButton": "see my other work",
-	"buyCoffeeButton": "buy me a coffee",
-	"replayButton": "replay this mess",
+	"otherWorkButton": "περισσότερα από τον δημιουργό",
+	"buyCoffeeButton": "patreon ου δημιουργού",
+	"replayButton": "ξαναπαίξε αυτό το χάος",
 
 	// (logo.png) Post Credits
-	"logoWBWWB": "SHARE THE PAIN:",
+	"logoWBWWB": "ΜΟΙΡΑΣΟΥ ΤΟΝ ΠΟΝΟ:",
 
 	// (end_prototype.png)
-	"endOfPrototype": "END OF PROTOTYPE",
-	"toBeContinued": "(to be continued!)",
+	"endOfPrototype": "ΤΕΛΟΣ ΠΡΩΤΟΤΥΠΟΥ",
+	"toBeContinued": "(συνεχίζεται!)",
 
 	// (quote0002.png)
-	"WBWWB": "We become what we behold.",
-	"WSOTATOTSU": "We shape our tools and then our tools shape us.",
+	"WBWWB": "Γινόμαστε αυτό που παρατηρούμε.",
+	"WSOTATOTSU": "Σχηματίζουμε τα εργαλέια μας και μετά τα εργαλεία μας μα σχηματίζουν.",
 
 	// (quote0003.png)
-	"MarshallMcLuhan": "Marshall McLuhan",
+	"MarshallMcLuhan": "Μάρσαλ ΜακΛούχαν",
 
 	// (quote0004.png)
-	"misatrributed": "(misattributed)",
+	"misatrributed": "(με κακή απόδοση)",
 };
 
-// Umlaut codes: Ä = \xC4, Ö = \xD6, Ü = \xDC, ä = \xE4, ö = \xF6, ü = \xFC, ß = \x
-
-var textStrings_DE = {
-	// Act 1
-	"niceHat": "Oh, h\xFCbscher Hut!",
-	"outtaHere": "Ja, besser schnell weg hier.",
-	"getARoom": "Widerlich, nehmt euch ein Zimmer!",
-	"notCoolAnymore": "Schon geh\xF6rt? H\xFCte sind out.",
-	"tvOnTv": "Ein Fernseher! Im Fernsehen!",
-	"cricky": "Ein Grash\xFCpfer!",
-	"tooManyCrickets": "Ok, zu viele Grash\xFCpfer.",
-	"normalPeep": "Ein gew\xF6hnlicher Typ.",
-	"normalPeeps": "Ein paar gew\xF6hnliche Typen.",
-	"wowNothing": "Wow! Nichts! Gar nix!",
-
-	// Act 2
-	"crazySquareAttacks": "Verr\xFCcktes Quadrat greift an!",
-	"justMissed": "Ooooh, knapp verpasst.",
-	"somethingInteresting": "(laaangweilig *g\xE4hn*)",
-	"whoIsScreaming": "(*wer* schreit sie denn an?)",
-	"circleFearsSquares": "KREIS F\xDCRCHTET QUADRATE!",
-	"whoScaresThem": "(*wen* f\xFCrchten sie denn?)",
-	"areTheyScared": "(erwische sie, *wenn* sie fl\xFCchten)",
-	"squaresSnubCircles": "QUADRAT: Anschuldigungen von KREISEN l\xE4cherlich.",
-	"areTheySnubbed": "(erwische sie, *w\xE4hrend* sie angreifen)",
-	"everyoneHates": "ALLE HASSEN JEDEN!!1!",
-	"almostEveryoneHates": "FAST JEDER HASST ALLE...",
-	"squaresHateCircles": "QUADRATE HASSEN KREISE",
-	"circlesHateSquares": "KREISE HASSEN QUADRATE",
-	"areTheyYelling": "(erwische sie, *w\xE4hrend* sie sich gegenseitig anschreien)",
-	"nerdsNow": "Was machen diese Nerds jetzt?",
-	"schockedPeep": "Warum ist er geschockt?",
-	"whatever": "Egal...",
-
-	// Act 3
-	"ellipsis": ". . .",
-	"coolNoMore": "H\xFCte sind out. Hast du's immer noch nicht geh\xF6rt?",
-	"beScared": "HABT ANGST! SEID W\xDCTEND!",
-
-	// MANIFESTO
-	"manifesto": [
-		//"Als ob ihr an guten Nachrichten interessiert seid."     ,
-		"Wer will denn sehen, wie Leute miteinender klarkommen?",
-		"Frieden ist langweilig. Die Leute wollen Gewalt sehen.",
-		//"Frieden ist langweilig. Konflikt bringt Klicks."        ,
-		"Jede gute Story braucht einen Konflikt, also...",
-		//"...GIB DEM PUBLIKUM WAS ES WILL!"                       ,
-		"GIB DEM PUBLIKUM WAS ES WILL!"
-	],
-
-	// -- Strings currently in graphics resources --
-
-	// (bg_preload.png) Preload screen
-	"playingTime": "Spielzeit: 5 Minuten",
-	"warning": "Warnung: die folgende Sendung\nenth\xE4lt Darstellungen von Arroganz,\nBeleidigungen & Gewalt.\nund ist nicht f\xFCr Kinder geeignet.",
-
-	// (cam-instructions.png) Camera Instructions
-	"pointAndClick": "POINT & CLICK", // I'd leave that in English, even for the German translation. /stefan
-
-	// (chyron3.png)
-	"chyronNothing": "Oh wow, *NICHTS*!",
-
-	// (credits0001.png)
-	"createdBy": "created by", // I'd leave that in English, even for the German translation. /stefan
-	"NickyCase": "Nicky Case", // name; just here in case you want to display text instead of graphic. /stefan
-
-	// (credits0002.png)
-	"manyThanks": "Vielen Dank an alle Tester:",
-
-	// (credits000[3,4,5,6].png)
-	"patreonSupporters": "und an meine Patreon Unterst\xFCtzer:",
-
-	// (credits0007.png)
-	"lastButNotLeast": "und zu guter Letzt,",
-
-	// (credits0008.png)
-	"thankYouForPlaying": "Danke an DICH f\xFCr's spielen!",
-
-	// (preload_play.png)
-	"playButton": "START",
-
-	// (end_button.png) Post Credits
-	"otherWorkButton": "andere Projekte",
-	"buyCoffeeButton": "kauf mir einen Kaffee",
-	"replayButton": "noch mal spielen",
-
-	// (logo.png) Post Credits
-	"logoWBWWB": "TEILE DEINEN SCHMERZ:",
-
-	// (end_prototype.png)
-	"endOfPrototype": "ENDE DES PROTOTYPS",
-	"toBeContinued": "(wird fortgesetzt!)",
-
-	// (quote0002.png)
-	"WBWWB": "We become what we behold.", // I'd leave that in English, even for the German translation. /stefan
-	"WSOTATOTSU": "Wir formen unsere Werkzeuge, und dann formen unsere Werkzeuge uns.",
-
-	// (quote0003.png)
-	"MarshallMcLuhan": "Marshall McLuhan", // name; just here in case you want to display text instead of graphic. /stefan
-
-	// (quote0004.png)
-	"misatrributed": "(falsch zugeschrieben)",
-};
-
-// Persian translation. همرنگ جماعت شو
-//
-// by Ali FZL https://twitter.com/ali_fzl95
-
-var textStrings_FA = {
-	// Act 1
-	"niceHat": "!به به! چه کلاه خفنی",
-	"outtaHere": "!برو گمشو بیرون",
-	"getARoom": "زوجین عزیز! میدونین گشت ارشاد چیه؟",
-	"notCoolAnymore": "!دیگه کلاه خز شده",
-	"tvOnTv": "! عجب ! تلوزیون تو تلوزیون",
-	"cricky": "^_^ گوگولی",
-	"tooManyCrickets": "!انقدر گوگولی هم دیگه نوبره",
-	"normalPeep": "یه بنده خدایِ معمولی",
-	"normalPeeps": "چندتا آدمِ معمولی",
-	"wowNothing": "عکاس کی بودی تو؟",
-
-	// Act 2
-	"crazySquareAttacks": "!کله مربعی هایِ وحشی ",
-	"justMissed": "!خیلی نزدیک بود",
-	"somethingInteresting": "!عکس ات باید یه موضوع جالب رو نشون بده",
-	"whoIsScreaming": "!تو عکس کسی که سرش داد میزنن هم باشه",
-	"circleFearsSquares": "!کله گرد ها از کله مربعی ها میترسن ",
-	"whoScaresThem": "از دست کی ترسیدن اینا؟",
-	"areTheyScared": "!وقتی از یه کله مربعی ترسیدن ازشون عکس بگیر", // not used
-	"squaresSnubCircles": "!کله مربعی ها کله گردها رو سرزنش می کنن ",
-	"areTheySnubbed": "!موقع سرزنش کردنشون ازشون عکس بگیر", // not used
-	"everyoneHates": "!الان همه از همه بدشون میاد",
-	"almostEveryoneHates": "!کم کم همه دارن از همدیگه بدشون میاد ",
-	"squaresHateCircles": "!کله مربعی ها حالشون از کله گردها بهم میخوره ",
-	"circlesHateSquares": "!کله گردها از کله مربعی ها متنفرن ",
-	"areTheyYelling": "!زمانیکه دارن سر همدیگه داد میزنن ازشون عکس بگیر", // not used
-	"nerdsNow": "ساقی شون کیه بالاغیرتا؟",
-	"schockedPeep": "چرا پشمای این بشر ریخته؟",
-	"whatever": "!به اونجام",
-
-	// Act 3
-	"ellipsis": ". . .",
-	"coolNoMore": "شنیدی میگن کلاه دیگه خز شده؟",
-	"beScared": "!یا ابوالفضل",
-
-	// MANIFESTO
-	"manifesto": [
-		//"as if you viewers want GOOD news"       ,
-		"!تماشای زندگی صلح آمیز خیلی جالب نیس دیگه",
-		"!صلح و شادی خز شده! وحشی گری باحاله الان",
-		"...هر داستانی هم که گره ای میخواد",
-		"!پس به مخاطب همون چیزی رو بده که میخواد"
-	],
-
-	// -- Strings currently in graphics resources --
-
-	// (bg_preload.png) Preload screen
-	"playingTime": "!زمان بازی: حداکثر 5 دقیقه ",
-	"warning": "!هشدار! این بازی حاوی صحنه خشونت آمیز هست",
-
-	// (cam-instructions.png) Camera Instructions
-	"pointAndClick": "!کادر ببند و عکس بگیر",
-
-	// (chyron3.png)
-	"chyronNothing": "!عکس ات بدرد نمی خوره",
-
-	// (credits0001.png)
-	"createdBy": ":ترجمه از",
-	"NickyCase": "Ali Fazeli",
-
-	// (credits0002.png)
-	"manyThanks": " :ممنون از",
-
-	// (credits000[3,4,5,6].png)
-	"patreonSupporters": ":با تشکر از",
-
-	// (credits0007.png)
-	"lastButNotLeast": "...و در آخر",
-
-	// (credits0008.png)
-	"thankYouForPlaying": "!مرسی از شما دوست عزیز",
-
-	// (preload_play.png)
-	"playButton": "!بسم الله",
-
-	// (end_button.png) Post Credits
-	"otherWorkButton": "بقیه بازیاتون چیاس؟",
-	"buyCoffeeButton": "!یه قهوه برام بخر خب",
-	"replayButton": "دوباره بازی کنم؟",
-
-	// (logo.png) Post Credits
-	"logoWBWWB": "اشتراک گذاری",
-
-	// (end_prototype.png)
-	"endOfPrototype": "پایان شبیه سازی",
-	"toBeContinued": "...ادامه دارد",
-
-	// (quote0002.png)
-	"WBWWB": "!تو همان که می نَمایی هستی",
-	"WSOTATOTSU": "We shape our tools and then our tools shape us.",
-
-	// (quote0003.png)
-	"MarshallMcLuhan": "Marshall McLuhan",
-
-	// (quote0004.png)
-	"misatrributed": "(!با مقداری تغییر)",
-};
-
-var textStrings = textStrings_FA
-//var textStrings = textStrings_KR;
+var textStrings = textStrings_GR
