0162c372f6d8a1e194d703b1493a0960d0208c31
Labrini Batagianni <76888409+Galandrielle@users.noreply.github.com>
2022-06-21 23:12:21 +0300

    Update textStrings.js

--- js/textStrings.js
+++ js/textStrings.js
@@ -619,6 +619,108 @@ var textStrings_ES = {
 	"misatrributed": "(misattributed)",
 };
 
+var textStrings_GR = {
+	// Act 1
+	"niceHat": "ΩΡΑΙΟ ΚΑΠΕΛΟ",
+	"outtaHere": "ναι φύγε από δω",
+	"getARoom": "ΑΗΔΙΑ, ΝΑ ΠΑΤΕ ΑΛΛΟΥ",
+	"notCoolAnymore": "ναι τα καπέλα δεν είναι πια κουλ",
+	"tvOnTv": "ΤΗΛΕΟΡΑΣΗ... ΣΤΗΝ ΤΗΛΕΟΡΑΣΗ!",
+	"cricky": "ΤΡΙΖΕΙ ΛΙΓΟ <3",
+	"tooManyCrickets": "οκ πολλά τριζόνια",
+	"normalPeep": " απλώς ένας κανονικός τύπος",
+	"normalPeeps": "απλώς μερικοί κανονικοί τύποι",
+	"wowNothing": "ΟΥΑΟΥ, ΕΙΝΑΙ ΤΟ ΤΙΠΟΤΑ",
+
+	// Act 2
+	"crazySquareAttacks": "ΤΡΕΛΕΣ ΕΠΙΘΕΣΕΙΣ ΤΕΤΡΑΓΩΝΩΝ",
+	"justMissed": "ωωωωωω μόλις το έχασες",
+	"somethingInteresting": "(πρέπει να τους πιάσεις να κάνουν ΚΑΤΙ ενδιαφέρον...)",
+	"whoIsScreaming": "(πρέπει να πιάσεις αυτόν που τους φωνάζει)",
+	"circleFearsSquares": "ΚΥΚΛΟΣ ΦΟΒΑΤΑΙ ΤΑ ΤΕΤΡΑΓΩΝΑ",
+	"whoScaresThem": "(πρέπει επίσης να πιάσεις ΠΟΙΟΝ φοβούνται...)",
+	"areTheyScared": "(πρέπει να τους πιάσεις να ΦΟΒΟΥΝΤΑΙ από τετράγωνο)", // not used
+	"squaresSnubCircles": "ΤΑ ΤΕΤΡΑΓΩΝΑ ΣΝΟΜΠΑΡΟΥΝ ΤΟΥΣ ΚΥΚΛΟΥΣ",
+	"areTheySnubbed": "(πρέπει να τους πιάσεις ενώ σνομπάρουν κύκλο...)", // not used
+	"everyoneHates": "ΟΛΟΙ ΜΙΣΟΥΝ ΟΛΟΥΣ!!1!",
+	"almostEveryoneHates": " ΣΧΕΔΟΝ ΟΛΟΙ ΜΙΣΟΥΝ ΟΛΟΥΣ...",
+	"squaresHateCircles": "ΤΑ ΤΕΤΡΑΓΩΝΑ ΜΙΣΟΥΝ ΤΟΥΣ ΚΥΚΛΟΥΣ",
+	"circlesHateSquares": "ΟΙ ΚΥΚΛΟΙ ΜΙΣΟΥΝ ΤΑ ΤΕΤΡΑΓΩΝΑ"",
+	"areTheyYelling": "(πρέπει να τους πιάσεις να ΦΩΝΑΖΟΥΝ σε άλλους)", // not used
+	"nerdsNow": "τί κάνουν αυτοί οι σπασίκλες τώρα",
+	"schockedPeep": "γιατί είναι αυτός o τύπος σοκαρισμένος;",
+	"whatever": "τέλος πάντων",
+
+	// Act 3
+	"ellipsis": ". . .",
+	"coolNoMore": "τα καπέλα δεν είναι πια κουλ, δεν άκουσες;",
+	"beScared": "ΤΡΟΜΑΞΕ. ΘΥΜΩΣΕ.",
+
+	// MANIFESTO
+	"manifesto": [
+		//"λες κι εσείς οι θεατές θέλετε ΚΑΛΑ νέα"       ,
+		"ποιος μπαίνει να δει *ανθρώπους να τα πηγαίνουν καλά*;",
+		"η ειρήνη είναι βαρετή. η βία γίνεται viral.",
+		//"η ειρήνη είναι βαρετή. η σύγκρουση φέρνει κλικς." ,
+		"και κάθε ιστορία χρειάζεται μια σύγκρουση, οπότε...",
+		//"...ΔΩΣΕ ΣΤΟ ΚΟΙΝΟ ΑΥΤΟ ΠΟΥ ΘΕΛΕΙ."   ,
+		"ΔΩΣΕ ΣΤΟ ΚΟΙΝΟ ΑΥΤΟ ΠΟΥ ΘΕΛΕΙ."
+	],
+
+	// -- Strings currently in graphics resources --
+
+	// (bg_preload.png) Preload screen
+	"playingTime": "χρόνος παιχνιδιού: 5 λεπτά",
+	"warning": "προειδοποίηση: το πρόγραμμα\nπεριέχει σκηνές ακαταδεξίας,\nαγένειας και μαζικής δολοφονίας.\nη σύνεση του θεατή είναι προτεινόμενη.\nΔημιουργός: Nicky Case, Μετάφραση στα Ελληνικά: Λαμπρινή Μπαταγιάννη",
+
+	// (cam-instructions.png) Camera Instructions
+	"pointAndClick": "POINT & CLICK",
+
+	// (chyron3.png)
+	"chyronNothing": "ΟΥΑΟΥ, ΕΙΝΑΙ ΤΟ *ΤΙΠΟΤΑ*",
+
+	// (credits0001.png)
+	"createdBy": "δημιουργός",
+	"NickyCase": "Νίκι Κέις",
+
+	// (credits0002.png)
+	"manyThanks": "Ο Νίκι Κέις ευχαριστεί τους δοκιμαστές του παιχνιδιού:",
+
+	// (credits000[3,4,5,6].png)
+	"patreonSupporters": "και τους υποστηρικτές στο patreon:",
+
+	// (credits0007.png)
+	"lastButNotLeast": "και τελευταία αλλά όχι καταϊδρωμένα,",
+
+	// (credits0008.png)
+	"thankYouForPlaying": "ευχαριστεί ΕΣΕΝΑ που έπαιξες!",
+
+	// (preload_play.png)
+	"playButton": "ΠΑΙΞΕ",
+
+	// (end_button.png) Post Credits
+	"otherWorkButton": "περισσότερα από τον Νίκι Κέις",
+	"buyCoffeeButton": "patreon του Νίκι Κέις",
+	"replayButton": "ξαναπαίξε αυτό το χάος",
+
+	// (logo.png) Post Credits
+	"logoWBWWB": "ΜΟΙΡΑΣΟΥ ΤΟΝ ΠΟΝΟ:",
+
+	// (end_prototype.png)
+	"endOfPrototype": "ΤΕΛΟΣ ΠΡΩΤΟΤΥΠΟΥ",
+	"toBeContinued": "(συνεχίζεται!)",
+
+	// (quote0002.png)
+	"WBWWB": "Γινόμαστε αυτό που παρατηρούμε.",
+	"WSOTATOTSU": "Σχηματίζουμε τα εργαλεία μας και μετά τα εργαλεία μας μας σχηματίζουν.",
+
+	// (quote0003.png)
+	"MarshallMcLuhan": "Μάρσαλ ΜακΛούχαν",
+
+	// (quote0004.png)
+	"misatrributed": "(με κακή απόδοση)",
+};
+
 const textStrings = (() => {
 	switch (navigator.language) {
 		case "en-US":
@@ -633,6 +735,8 @@ const textStrings = (() => {
 			return textStrings_PT_BR;
 		case "es":
 			return textStrings_ES;
+		case "es":
+			return textStrings_GR;
 		default:
 			return textStrings_EN;
 	}
